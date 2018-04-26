0f084a24278eb4a036770930c6fef1e118ff54a7
Fuxi <327714392@qq.com>
2018-04-26 09:09:38 +0800

    Signed-off-by: Fuxi <327714392@qq.com> 更新了数据库配置~

--- application/database.php
+++ application/database.php
@@ -13,9 +13,9 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '39.106.150.169',
     // 数据库名
-    'database'        => '',
+    'database'        => 'nweoson',
     // 用户名
     'username'        => 'root',
     // 密码
