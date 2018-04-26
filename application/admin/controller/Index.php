2ab662cce94701328973c38396263e4740d394fe
Fuxi <327714392@qq.com>
2018-04-26 19:21:20 +0800

    Signed-off-by: Fuxi <327714392@qq.com> 跟新代码@！！

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -6,10 +6,10 @@ class Index  extends Controller
 {
     public function index()
     {
-//        $ip    =  $_SERVER['SERVER_ADDR'];
-//        if($ip!='127.0.0.1'){
-//            die("<script>alert('抱歉,本网站暂不对外开放')</script>");
-//        }
+        $ip    =  $_SERVER['SERVER_ADDR'];
+        if($ip!='127.0.0.1'){
+            die("<script>alert('抱歉,本网站暂不对外开放')</script>");
+        }
 
      }
 
--- application/admin/model/ConfgModel.php
+++ application/admin/model/ConfgModel.php
@@ -5,9 +5,12 @@ use think\Db;
 class ConfgModel extends Model
 {
     public $tableName = "oson_config";
-    public function Find($str = "")
-    {
+    public function Find($str = ""){
         $res = Db::table($this->tableName)->where("is_show",$str)->find();
+        return $res;
+    }
+    public function logAdd($logArr){
+        $res = Db::table("oson_log")->insert($logArr);
         return $res;
     }
 }
